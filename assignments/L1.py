ddf33314de37cd8067fa6199e98654c239ba7408
VeKo <veko@adwarewizards.com>
2024-02-24 12:39:33 +0100

    Added l1&2

---
+++ assignments/L1.py
@@ -0,0 +1,42 @@
+def print_line(i):
+    for c in range(0,i):
+        print("*", end="")
+    print("")
+        
+
+print("Enter a number : ", end="")
+n = int(input())
+
+'''
+Examples:
+=============
+input: 1
+output: 
+
+=============
+input: 2
+output: 
+*
+=============
+input: 5
+output: 
+*
+**
+**
+*
+=============
+input: 10 
+output: 
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+'''
+
+for i in range(0, n):
+    # Add your code below this line
